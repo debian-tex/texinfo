2f1b822e3e328f173a964848dbccca7b038732d1
Norbert Preining <preining@debian.org>
2009-06-10 09:54:04 +0000

    merge the split-ii branch into trunk

--- debian/install-info.sh
+++ debian/install-info.sh
@@ -3,49 +3,22 @@
 # Wrapper to the GNU's install-info, to be compatible with the one that used to
 # be packaged by dpkg on Debian.
 #
-# (C) 2006 Nicolas François <nicolas.francois@centraliens.net>
-#
+# written by Norbert Preining, this is not copyrightable ;-)
 #
 set -e
 
+if [ -z "$DPKG_RUNNING_VERSION" ] ; then
+  # it seems we are running from outside a maintainer script, so give a
+  # warning and call ginstall-info without anything else
+  echo "This is not dpkg install-info anymore, but GNU install-info" >&2
+  echo "See the man page for ginstall-info for command line arguments" >&2
+  ginstall-info "$@"
+else 
+  # we are running from a maintainer script, simply ignore the call
+  # since we have trigger support and people should rebuild their
+  # package with new debhelper which does not add calls to install-info
+  echo "Ignoring install-info called from maintainer script" >&2
+  echo "The package $DPKG_MAINTSCRIPT_PACKAGE should be rebuild with new debhelper to get trigger support" >&2
+fi
 
-INSTALL_INFO=/usr/bin/ginstall-info
-INSTALL_INFO="echo $INSTALL_INFO"
 
-options=""
-while [ $# != 0 ]
-do
-	case "$1" in
-	--section)
-# Try to detect if --section has two arguments.
-# If the second argument is a file, then it is probably the name of the
-# info file.
-		shift
-		first_arg="$1"
-		shift
-		if [ -f "$1" ] || [ "${1#-}" != "$1" ]
-		then
-			options="$options --section \"$first_arg\""
-		else
-# FIXME: --section-regex needs to be impelmented.
-			options="$options --section \"$1\" --section-regex \"$first_arg\""
-			shift
-		fi
-		;;
-	--remove-exactly)
-# FIXME: This needs to be checked with the emacs packages
-#        (which use --remove-exactly).
-		options="$options --remove"
-		shift
-		;;
-	-*)
-		options="$options $1"
-		shift
-		;;
-	*)
-		options="$options \"$1\""
-		shift
-		;;
-	esac
-done
-/bin/sh -c "$INSTALL_INFO $options"

3e686080ab9a42a7d56232a697d0c22c6e503b45
Norbert Preining <preining@debian.org>
2009-09-23 23:51:19 +0000

    install-info.sh: do not warn on --remove and --remove-exactely

--- debian/install-info.sh
+++ debian/install-info.sh
@@ -17,6 +17,18 @@ else
   # we are running from a maintainer script, simply ignore the call
   # since we have trigger support and people should rebuild their
   # package with new debhelper which does not add calls to install-info
+  # Do not complain if called with "--remove" or "--remove-exactly",
+  # as these are used in old packages' prerm scripts (see #546165)
+  while [ -n "$1" ]; do
+    case "$1" in
+      --remove|--remove-exactly)
+        exit 0
+        ;;
+      *)
+        shift
+        ;;
+    esac
+  done
   echo "Ignoring install-info called from maintainer script" >&2
   echo "The package $DPKG_MAINTSCRIPT_PACKAGE should be rebuilt with new debhelper to get trigger support" >&2
 fi
